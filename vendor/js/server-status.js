70fff7611e8c2147cd78ea1ef5c038b3b5a04150
-kakara- <kakaradarkorbit69@gmail.com>
2018-10-12 15:55:35 +0300

    Skriptkiddie
    Vaihoin skriptit omiksi tiedostoiksi + poistin banlist.js, koska se oli mallipohja jota joskus käytin ku opettelin

---
+++ vendor/js/server-status.js
@@ -0,0 +1,26 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "https://use.gameapis.net/mc/query/players/151.80.78.212:28373.json";
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var stabox = this.responseText;
+        var kara = JSON.parse(stabox);
+        if (kara["status"] === true) {
+            document.getElementById("status").style.backgroundColor = "darkgreen";
+            document.getElementById("statusbor").style.border = "solid 4px darkgreen";
+            document.getElementById("online").innerHTML = kara.players["online"];
+            if (kara.players["online"] == "0") {
+                document.getElementById("online").innerHTML = "Pelaajia ei ole paikalla.";
+            }
+            document.getElementById("ip").innerHTML = "<p>karanteeni.net</p>" + "<p id='numip'>" + kara["hostname"] + ":" + kara["port"] + "&nbsp;<button class='btn btn-outline-dark' onclick='copyIp(document.getElementById(\"numip\"));'><i class='fas fa-copy'></i></button></p>";
+            document.getElementById("server").innerHTML = '<img src="./assets/images/server-icon.png"><p class="card-text" style="padding-top: 1rem;">Pelaajia paikalla:</p>';
+        }
+    } else {
+            document.getElementById("status").style.backgroundColor = "darkred";
+            document.getElementById("statusbor").style.border = "solid 4px darkred";
+            document.getElementById("online").innerHTML = "Pelaajia ei ole paikalla.";
+            document.getElementById("ip").innerHTML = "Osoitteen kysely epäonnistui. Palvelin ei ole päällä."
+            document.getElementById("server").innerHTML = '<img src="./assets/images/server-icon.png">';
+        }
+    };
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
