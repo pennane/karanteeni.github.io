0bb47e8b0f4e771b27957871e73922851534c290
-kakara- <kakaradarkorbit69@gmail.com>
2019-03-08 20:40:23 +0200

    v. 2.4.0
    - Navbarin tiivistäminen / paloittelu
    - Turhien tiedostojen poistaminen
    - Development -> Production.min

--- assets/components/navbar.jsx
+++ assets/components/navbar.jsx
@@ -1,60 +1,109 @@
 'use strict';
 
+function Icon(props) {
+    return (
+        <i className={props.icon}></i>
+    )
+}
+
+class NavLink extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: this.props.title,
+            icon: this.props.icon,
+            url: this.props.url
+        };
+    }
+
+    render() {
+        return (
+            <li className="nav-item">
+                <a href={this.props.url} className="nav-link">
+                    <Icon icon={this.state.icon} />&nbsp;&nbsp;{this.state.title}
+                </a>
+            </li>
+        )
+    }
+}
+
+class DropDownCollapseMenu extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: this.props.title,
+            target: this.props.dataTarget,
+            labelClassName: this.props.labelClassName
+        };
+    }
+
+    render() {
+        return(
+            <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-target={this.state.dataTarget} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i className={this.state.labelClassName}></i>&nbsp;&nbsp;{this.state.title}
+                </a>
+                    <div className="dropdown-menu collapse" aria-labelledby="navbarDropdown">
+                        <h6 className="dropdown-header">{this.state.title}</h6>
+                        {this.props.children}
+                    </div>
+            </li>
+        );
+    }
+}
+
+class DropDownItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: this.props.title,
+            icon: this.props.icon,
+            url: this.props.url
+        };
+    }
+
+    render() {
+        return (
+            <a className="dropdown-item" href={this.state.url}><Icon icon={this.state.icon} />&nbsp;&nbsp;{this.state.title}</a>
+        )
+    }
+}
+
 class Navbar extends React.Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark karanav fixed-top">
                 <div className="container">
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="nav navbar-nav nav-fill" style={{flexGrow: 1, textAlign: 'center'}}>
-                        <li className="nav-item">
-                        <a className="nav-link" href="index"><i className="fas fa-home"></i>&nbsp;&nbsp;Etusivu
-                        </a>
-                        </li>
-                        <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-target="#rc" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i className="fas fa-info"></i>&nbsp;&nbsp;Yleistietoa
-                        </a>
-                            <div className="dropdown-menu collapse" aria-labelledby="navbarDropdown">
-                            <h6 className="dropdown-header">Yleistietoa</h6>
-                            <a className="dropdown-item" href="yleistietoa"><i className="fas fa-info"></i>&nbsp;&nbsp;Yleistietoa</a>
-                            <a className="dropdown-item" href="recipes"><i className="fas fa-hammer"></i>&nbsp;&nbsp;Custom Recipet</a>
-                            <div className="dropdown-divider"></div>
-                            <a href="hoks" className="dropdown-item"><i className="fas fa-thumbs-up"></i>&nbsp;&nbsp;Hyvä tietää!</a>
-                            </div>
-                        </li>
-                        <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-target="#dd" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i className="fas fa-exclamation"></i>&nbsp;&nbsp;Säännöt
-                        </a>
-                            <div className="dropdown-menu collapse" aria-labelledby="navbarDropdown">
-                            <h6 className="dropdown-header">Säännöt</h6>
-                            <a className="dropdown-item" href="saannot"><i className="fas fa-cross"></i>&nbsp;&nbsp;Survivalin säännöt</a>
-                            <a className="dropdown-item" href="chat-saannot"><i className="fas fa-comments"></i>&nbsp;&nbsp;Chat-säännöt</a>
-                            </div>
-                        </li>
-                        <li className="nav-item">
-                            <a href="rankit" className="nav-link"><i className="fas fa-medal"></i>&nbsp;&nbsp;Rankit</a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i className="fas fa-users"></i>&nbsp;&nbsp;Ylläpito</a>
-                            <div className="dropdown-menu collapse" aria-labelledby="navbarDropdown" id="df">
-                            <h6 className="dropdown-header">Ylläpito</h6>
-                            <a className="dropdown-item" href="yllapito"><i className="fas fa-users"></i>&nbsp;&nbsp;Ylläpito</a>
-                            {/* <a className="dropdown-item" href="yphaku"><i class="fas fa-file-alt"></i>&nbsp;&nbsp;Hae ylläpitoon</a> */}
-                            </div>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="maailmat"><i className="fas fa-globe-africa"></i>&nbsp;&nbsp;Maailmat</a>
-                        </li>
-                        <li className="nav-item">
-                        <a className="nav-link" href="lahjoitus"><i className="fas fa-gift"></i>&nbsp;&nbsp;Lahjoitukset</a>
-                        </li>
-                    </ul>
+                        <ul className="nav navbar-nav nav-fill" style={{flexGrow: 1, textAlign: 'center'}}>
+                            <NavLink icon="fas fa-home" url="index" title="Etusivu" />
+                            
+                            <DropDownCollapseMenu title="Yleistietoa" dataTarget="#rc" labelClassName="fas fa-info">
+                                <DropDownItem icon="fas fa-info" url="yleistietoa" title="Yleistietoa" />
+                                <DropDownItem icon="fas fa-hammer" url="recipes" title="Custom recipet" />
+                                <DropDownItem icon="fas fa-thumbs-up" url="hoks" title="Hyvä tietää!" />
+                            </DropDownCollapseMenu>
+
+                            <DropDownCollapseMenu title="Säännöt" dataTarget="#dd" labelClassName="fas fa-exclamation">
+                                <DropDownItem icon="fas fa-cross" url="saannot" title="Survivalin säännöt" />
+                                <DropDownItem icon="fas fa-comments" url="chat-saannot" title="Chat-säännöt" />
+                            </DropDownCollapseMenu>
+
+                            <NavLink icon="fas fa-medal" url="rankit" title="Rankit" />
+                            
+                            <DropDownCollapseMenu title="Ylläpito" labelClassName="fas fa-users">
+                                <DropDownItem icon="fas fa-users" url="yllapito" title="Ylläpito" />
+                            </DropDownCollapseMenu>
+
+                            <NavLink icon="fas fa-globe-africa" url="maailmat" title="Maailmat" />
+                            <NavLink icon="fas fa-gift" url="lahjoitus" title="Lahjoitukset" />
+                        </ul>
                     </div>
                 </div>
             </nav>
