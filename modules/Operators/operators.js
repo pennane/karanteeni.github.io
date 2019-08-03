fecd3a23a95e89ac2ff233dd5155d6a5bd6d2881
Arttu Pennanen <arttu@pennanen.org>
2019-08-03 14:58:19 +0300

    Tyylittely pois js tiedostosta

--- modules/Operators/operators.js
+++ modules/Operators/operators.js
@@ -19,6 +19,22 @@ var admin = document.getElementById('Adminit');
 var op = document.getElementById('Operaattorit');
 var mod = document.getElementById('Moderaattorit');
 
+function createYpImageElement(uuid, cssClass) {
+  var ypImg = document.createElement('div');
+  ypImg.className = 'yp-img ' + cssClass;
+
+  var ypskin = document.createElement('img');
+  ypskin.className = 'card-img-top';
+  ypskin.style.width = '100px';
+  ypskin.style.marginTop = '1.2em';
+  ypskin.style.marginBottom = '1.2em';
+  ypskin.style.height = '225px';
+  ypskin.setAttribute('src', 'https://crafatar.com/renders/body/' + uuid);
+
+  ypImg.appendChild(ypskin)
+  return ypImg;
+}
+
 function giveOp(jsonObj) {
 
     var owners = jsonObj['omistajat'];
@@ -38,35 +54,16 @@ function giveOp(jsonObj) {
         col.style.marginBottom = '20px';
   
         var card = document.createElement('div');
-        card.className = 'card';
-        card.style.width = '18rem';
-        card.style.border = '2px solid red';
-        card.style.borderRadius = '0.80rem';
-        card.style.backgroundColor = '#fff993';
-        card.style.color = 'black';
-        card.style.margin = 'auto';
+        card.className = 'card yp-card';
   
         var cardTitle = document.createElement('h5');
-        cardTitle.className = 'card-title';
-        cardTitle.style.paddingTop = '15px';
+        cardTitle.className = 'card-title yp-card-title';
         cardTitle.textContent = owners[i].nimi;
   
-        var ypImg = document.createElement('div');
-        ypImg.className = 'yp-img';
-  
-        var ypskin = document.createElement('img');
-        ypskin.className = 'card-img-top';
-        ypskin.style.width = '100px';
-        ypskin.style.marginTop = '1.2em';
-        ypskin.style.marginBottom = '1.2em';
-        ypskin.style.height = '225px';
-        ypskin.setAttribute('src', 'https://crafatar.com/renders/body/' + owners[i].uuid);
+        var ypImg = createYpImageElement(owners[i].uuid, "owner")
   
         var cardBody = document.createElement('div');
-        cardBody.className = 'card-body';
-        cardBody.style.backgroundColor = '#fff993';
-        cardBody.style.color = 'black';
-        cardBody.style.borderRadius = '0.80rem';
+        cardBody.className = 'card-body yp-card-body';
   
         var cardText = document.createElement('p');
         cardText.className = 'card-text';
@@ -76,7 +73,6 @@ function giveOp(jsonObj) {
         col.appendChild(card);
         card.appendChild(cardTitle);
         card.appendChild(ypImg);
-        ypImg.appendChild(ypskin);
         card.appendChild(cardBody);
         cardBody.appendChild(cardText);
   
@@ -112,35 +108,16 @@ function giveOp(jsonObj) {
           col.style.marginBottom = '20px';
     
           var card = document.createElement('div');
-          card.className = 'card';
-          card.style.width = '18rem';
-          card.style.border = '2px solid red';
-          card.style.borderRadius = '0.80rem';
-          card.style.backgroundColor = '#fff993';
-          card.style.color = 'black';
-          card.style.margin = 'auto';
+          card.className = 'card yp-card';
     
           var cardTitle = document.createElement('h5');
-          cardTitle.className = 'card-title';
-          cardTitle.style.paddingTop = '15px';
+          cardTitle.className = 'card-title yp-card-title';
           cardTitle.textContent = admins[i].nimi;
     
-          var ypImg = document.createElement('div');
-          ypImg.className = 'yp-img';
-    
-          var ypskin = document.createElement('img');
-          ypskin.className = 'card-img-top';
-          ypskin.style.width = '100px';
-          ypskin.style.marginTop = '1.2em';
-          ypskin.style.marginBottom = '1.2em';
-          ypskin.style.height = '225px';
-          ypskin.setAttribute('src', 'https://crafatar.com/renders/body/' + admins[i].uuid);
+          var ypImg = createYpImageElement(admins[i].uuid, "admin")
     
           var cardBody = document.createElement('div');
-          cardBody.className = 'card-body';
-          cardBody.style.backgroundColor = '#fff993';
-          cardBody.style.color = 'black';
-          cardBody.style.borderRadius = '0.80rem';
+          cardBody.className = 'card-body yp-card-body';
     
           var cardText = document.createElement('p');
           cardText.className = 'card-text';
@@ -150,7 +127,6 @@ function giveOp(jsonObj) {
           col.appendChild(card);
           card.appendChild(cardTitle);
           card.appendChild(ypImg);
-          ypImg.appendChild(ypskin);
           card.appendChild(cardBody);
           cardBody.appendChild(cardText);
     
@@ -183,35 +159,16 @@ function giveOp(jsonObj) {
         col.style.marginBottom = '20px';
   
         var card = document.createElement('div');
-        card.className = 'card';
-        card.style.width = '18rem';
-        card.style.border = '2px solid red';
-        card.style.borderRadius = '0.80rem';
-        card.style.backgroundColor = '#fff993';
-        card.style.color = 'black';
-        card.style.margin = 'auto';
+        card.className = 'card yp-card';
   
         var cardTitle = document.createElement('h5');
-        cardTitle.className = 'card-title';
-        cardTitle.style.paddingTop = '15px';
+        cardTitle.className = 'card-title yp-card-title';
         cardTitle.textContent = smods[i].nimi;
   
-        var ypImg = document.createElement('div');
-        ypImg.className = 'yp-img';
-  
-        var ypskin = document.createElement('img');
-        ypskin.className = 'card-img-top';
-        ypskin.style.width = '100px';
-        ypskin.style.marginTop = '1.2em';
-        ypskin.style.marginBottom = '1.2em';
-        ypskin.style.height = '225px';
-        ypskin.setAttribute('src', 'https://crafatar.com/renders/body/' + smods[i].uuid);
+        var ypImg = createYpImageElement(smods[i].uuid, "smod")
   
         var cardBody = document.createElement('div');
-        cardBody.className = 'card-body';
-        cardBody.style.backgroundColor = '#fff993';
-        cardBody.style.color = 'black';
-        cardBody.style.borderRadius = '0.80rem';
+        cardBody.className = 'card-body yp-card-body';
   
         var cardText = document.createElement('p');
         cardText.className = 'card-text';
@@ -221,7 +178,6 @@ function giveOp(jsonObj) {
         col.appendChild(card);
         card.appendChild(cardTitle);
         card.appendChild(ypImg);
-        ypImg.appendChild(ypskin);
         card.appendChild(cardBody);
         cardBody.appendChild(cardText);
   
@@ -255,35 +211,16 @@ function giveOp(jsonObj) {
         col.style.marginBottom = '20px';
   
         var card = document.createElement('div');
-        card.className = 'card';
-        card.style.width = '18rem';
-        card.style.border = '2px solid red';
-        card.style.borderRadius = '0.80rem';
-        card.style.backgroundColor = '#fff993';
-        card.style.color = 'black';
-        card.style.margin = 'auto';
+        card.className = 'card yp-card';
   
         var cardTitle = document.createElement('h5');
-        cardTitle.className = 'card-title';
-        cardTitle.style.paddingTop = '15px';
+        cardTitle.className = 'card-title yp-card-title';
         cardTitle.textContent = mods[i].nimi;
   
-        var ypImg = document.createElement('div');
-        ypImg.className = 'yp-img';
-  
-        var ypskin = document.createElement('img');
-        ypskin.className = 'card-img-top';
-        ypskin.style.width = '100px';
-        ypskin.style.marginTop = '1.2em';
-        ypskin.style.marginBottom = '1.2em';
-        ypskin.style.height = '225px';
-        ypskin.setAttribute('src', 'https://crafatar.com/renders/body/' + mods[i].uuid);
+        var ypImg = createYpImageElement(mods[i].uuid, "mod")
   
         var cardBody = document.createElement('div');
-        cardBody.className = 'card-body';
-        cardBody.style.backgroundColor = '#fff993';
-        cardBody.style.color = 'black';
-        cardBody.style.borderRadius = '0.80rem';
+        cardBody.className = 'card-body yp-card-body';
   
         var cardText = document.createElement('p');
         cardText.className = 'card-text';
@@ -293,7 +230,6 @@ function giveOp(jsonObj) {
         col.appendChild(card);
         card.appendChild(cardTitle);
         card.appendChild(ypImg);
-        ypImg.appendChild(ypskin);
         card.appendChild(cardBody);
         cardBody.appendChild(cardText);
   
@@ -329,35 +265,16 @@ function giveOp(jsonObj) {
           col.style.marginBottom = '20px';
     
           var card = document.createElement('div');
-          card.className = 'card';
-          card.style.width = '18rem';
-          card.style.border = '2px solid red';
-          card.style.borderRadius = '0.80rem';
-          card.style.backgroundColor = '#fff993';
-          card.style.color = 'black';
-          card.style.margin = 'auto';
+          card.className = 'card yp-card';
     
           var cardTitle = document.createElement('h5');
-          cardTitle.className = 'card-title';
-          cardTitle.style.paddingTop = '15px';
+          cardTitle.className = 'card-title yp-card-title';
           cardTitle.textContent = builders[i].nimi;
     
-          var ypImg = document.createElement('div');
-          ypImg.className = 'yp-img';
-    
-          var ypskin = document.createElement('img');
-          ypskin.className = 'card-img-top';
-          ypskin.style.width = '100px';
-          ypskin.style.marginTop = '1.2em';
-          ypskin.style.marginBottom = '1.2em';
-          ypskin.style.height = '225px';
-          ypskin.setAttribute('src', 'https://crafatar.com/renders/body/' + builders[i].uuid);
+          var ypImg = createYpImageElement(builders[i].uuid, "builder")
     
           var cardBody = document.createElement('div');
-          cardBody.className = 'card-body';
-          cardBody.style.backgroundColor = '#fff993';
-          cardBody.style.color = 'black';
-          cardBody.style.borderRadius = '0.80rem';
+          cardBody.className = 'card-body yp-card-body';
     
           var cardText = document.createElement('p');
           cardText.className = 'card-text';
@@ -367,7 +284,6 @@ function giveOp(jsonObj) {
           col.appendChild(card);
           card.appendChild(cardTitle);
           card.appendChild(ypImg);
-          ypImg.appendChild(ypskin);
           card.appendChild(cardBody);
           cardBody.appendChild(cardText);
   
