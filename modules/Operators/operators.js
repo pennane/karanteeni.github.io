bb51657e52e2b4cb1b2c10f0d597c0edc0848dd0
Arttu Pennanen <arttu@pennanen.org>
2019-08-03 19:05:57 +0300

    Ylläpitosivu update

--- modules/Operators/operators.js
+++ modules/Operators/operators.js
@@ -4,13 +4,13 @@ request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
-request.onload = function() {
-    var operators = request.response;
-    giveOp(operators);
-    giveAdm(operators);
-    giveOperator(operators);
-    giveMod(operators);
-    giveBuild(operators);
+
+request.onload = function () {
+  var data = request.response;
+  Object.keys(data).forEach(function (type) {
+    var list = data[type]
+    createOperatorsDiv(list, type)
+  })
 }
 
 var builder = document.getElementById('Rakentajat');
@@ -19,281 +19,129 @@ var admin = document.getElementById('Adminit');
 var op = document.getElementById('Operaattorit');
 var mod = document.getElementById('Moderaattorit');
 
-function createYpImageElement(uuid, cssClass) {
-  var ypImg = document.createElement('div');
-  ypImg.className = 'yp-img ' + cssClass;
-
-  var ypskin = document.createElement('img');
-  ypskin.className = 'card-img-top';
-  ypskin.style.width = '100px';
-  ypskin.style.marginTop = '1.2em';
-  ypskin.style.marginBottom = '1.2em';
-  ypskin.style.height = '225px';
-  ypskin.setAttribute('src', 'https://crafatar.com/renders/body/' + uuid);
-
-  ypImg.appendChild(ypskin)
-  return ypImg;
+function getDestination(type) {
+  var destination;
+  switch (type) {
+    case "omistajat":
+      destination = owner
+      break;
+    case "adminit":
+      destination = admin
+      break;
+    case "operaattorit":
+      destination = op
+      break;
+    case "moderaattorit":
+      destination = mod
+      break;
+    case "rakentajat":
+      destination = builder
+      break;
+    default:
+      throw new Error("Could not resolve destination element")
+  }
+  return destination
 }
 
-function giveOp(jsonObj) {
-
-    var owners = jsonObj['omistajat'];
-
-    if (owners.length != 0) {
-      var hr = document.createElement('hr');
-      var br = document.createElement('br');
-  
-      var row = document.createElement('div');
-      row.className = 'row';
-      row.style.textAlign = 'center';
-          
-      for (var i = 0; i < owners.length; i++) {
-  
-        var col = document.createElement('div');
-        col.className = 'col-sm';
-        col.style.marginBottom = '20px';
-  
-        var card = document.createElement('div');
-        card.className = 'card yp-card owner';
-  
-        var cardTitle = document.createElement('h5');
-        cardTitle.className = 'card-title yp-card-title owner';
-        cardTitle.textContent = owners[i].nimi;
-  
-        var ypImg = createYpImageElement(owners[i].uuid, "owner")
-  
-        var cardBody = document.createElement('div');
-        cardBody.className = 'card-body yp-card-body owner';
-  
-        var cardText = document.createElement('p');
-        cardText.className = 'card-text';
-        cardText.textContent = owners[i].kuvaus;
-    
-        row.appendChild(col);
-        col.appendChild(card);
-        card.appendChild(cardTitle);
-        card.appendChild(ypImg);
-        card.appendChild(cardBody);
-        cardBody.appendChild(cardText);
-  
-        owner.parentNode.insertBefore(row, owner.nextSibling);
-        row.parentNode.insertBefore(hr, owner.nextSibling);
-        row.parentNode.insertBefore(br, hr.nextSibling);
-        
-      }
-    } else {
-      var h4 = document.createElement('h4');
-      h4.innerHTML = '#Sellout';
-      owner.parentNode.insertBefore(h4, owner.nextSibling);
-    }
-  }
-
-  function giveAdm(jsonObj) {
-
-    var hr = document.createElement('hr');
-    var br = document.createElement('br');
-
-    var admins = jsonObj['adminit'];
-
-    if (admins.length != 0) {
-      var row = document.createElement('div');
-      row.className = 'row';
-      row.style.textAlign = 'center';
-          
-      
-      for (var i = 0; i < admins.length; i++) {
-  
-          var col = document.createElement('div');
-          col.className = 'col-sm';
-          col.style.marginBottom = '20px';
-    
-          var card = document.createElement('div');
-          card.className = 'card yp-card';
-    
-          var cardTitle = document.createElement('h5');
-          cardTitle.className = 'card-title yp-card-title admin';
-          cardTitle.textContent = admins[i].nimi;
-    
-          var ypImg = createYpImageElement(admins[i].uuid, "admin")
-    
-          var cardBody = document.createElement('div');
-          cardBody.className = 'card-body yp-card-body admin';
-    
-          var cardText = document.createElement('p');
-          cardText.className = 'card-text';
-          cardText.textContent = admins[i].kuvaus;
-      
-          row.appendChild(col);
-          col.appendChild(card);
-          card.appendChild(cardTitle);
-          card.appendChild(ypImg);
-          card.appendChild(cardBody);
-          cardBody.appendChild(cardText);
-    
-          admin.parentNode.insertBefore(row, admin.nextSibling)
-          row.parentNode.insertBefore(hr, admin.nextSibling);
-          row.parentNode.insertBefore(br, hr.nextSibling);
-      }
-    } else {
-      var h4 = document.createElement('h4');
-      h4.innerHTML = 'Ketään ei ole katsomassa Operaattoreiden perään. :(';
-      admin.parentNode.insertBefore(h4, admin.nextSibling);
-    }
-  }
-
-    function giveOperator(jsonObj) {
-
-    var smods = jsonObj['operaattorit'];
-
-    if (smods.length != 0) {
-      var hr = document.createElement('hr');
-      var br = document.createElement('br');
-  
-      var row = document.createElement('div');
-      row.className = 'row';
-      row.style.textAlign = 'center';
-  
-      for (var i = 0; i < smods.length; i++) {
-        var col = document.createElement('div');
-        col.className = 'col-sm';
-        col.style.marginBottom = '20px';
-  
-        var card = document.createElement('div');
-        card.className = 'card yp-card smod';
-  
-        var cardTitle = document.createElement('h5');
-        cardTitle.className = 'card-title yp-card-title smod';
-        cardTitle.textContent = smods[i].nimi;
-  
-        var ypImg = createYpImageElement(smods[i].uuid, "smod")
-  
-        var cardBody = document.createElement('div');
-        cardBody.className = 'card-body yp-card-body smod';
-  
-        var cardText = document.createElement('p');
-        cardText.className = 'card-text';
-        cardText.textContent = smods[i].kuvaus;
-    
-        row.appendChild(col);
-        col.appendChild(card);
-        card.appendChild(cardTitle);
-        card.appendChild(ypImg);
-        card.appendChild(cardBody);
-        cardBody.appendChild(cardText);
-  
-        op.parentNode.insertBefore(row, op.nextSibling);
-        row.parentNode.insertBefore(hr, op.nextSibling);
-        row.parentNode.insertBefore(br, hr.nextSibling);
-      }
-    } else {
-      var h4 = document.createElement('h4');
-      h4.innerHTML = 'Operaattorit ovat karanneet jonnekin! Ehkä moderaattoreista voi joskus löytyä kelvollinen...';
-      op.parentNode.insertBefore(h4, op.nextSibling);
-    }
-  }
-
-  function giveMod(jsonObj) {
-
-    var mods = jsonObj['moderaattorit'];
-
-    if (mods.length != 0) {
-      var hr = document.createElement('hr');
-      var br = document.createElement('br');
-  
-      var row = document.createElement('div');
-      row.className = 'row';
-      row.style.textAlign = 'center';
-  
-      /* */for (var i = 0; i < mods.length; i++) {
-  
-        var col = document.createElement('div');
-        col.className = 'col-sm';
-        col.style.marginBottom = '20px';
-  
-        var card = document.createElement('div');
-        card.className = 'card yp-card mod';
-  
-        var cardTitle = document.createElement('h5');
-        cardTitle.className = 'card-title yp-card-title mod';
-        cardTitle.textContent = mods[i].nimi;
-  
-        var ypImg = createYpImageElement(mods[i].uuid, "mod")
-  
-        var cardBody = document.createElement('div');
-        cardBody.className = 'card-body yp-card-body mod';
-  
-        var cardText = document.createElement('p');
-        cardText.className = 'card-text';
-        cardText.textContent = mods[i].kuvaus;
-    
-        row.appendChild(col);
-        col.appendChild(card);
-        card.appendChild(cardTitle);
-        card.appendChild(ypImg);
-        card.appendChild(cardBody);
-        cardBody.appendChild(cardText);
-  
-        mod.parentNode.insertBefore(row, mod.nextSibling);
-        row.parentNode.insertBefore(hr, mod.nextSibling);
-        row.parentNode.insertBefore(br, hr.nextSibling);
-      }
-    } else {
-      var h4 = document.createElement('h4');
+function createEmptyListText(type, destination) {
+  var h4 = document.createElement('h4');
+  var destination;
+  switch (type) {
+    case "omistajat":
+      h4.textContent = '#Sellout';
+      break;
+    case "adminit":
+      h4.textContent = 'Ketään ei ole katsomassa Operaattoreiden perään. :(';
+      break;
+    case "operaattorit":
+      h4.textContent = 'Operaattorit ovat karanneet jonnekin! Ehkä moderaattoreista voi joskus löytyä kelvollinen...';
+      break;
+    case "moderaattorit":
       h4.innerHTML = 'Oh noes! Moderators not found. Ehkä sinun kannattaisi <a id="link" href="yphaku">hakea</a>!';
-      mod.parentNode.insertBefore(h4, mod.nextSibling);
+      break;
+    case "rakentajat":
+      h4.innerHTML = 'Katastrofaalista! Rakentajat ovat lakossa. Jos sinulta löytyy taitoa, voisit <a id="link" href="yphaku">hakea</a>!';
+      break;
+    default:
+      break;
   }
-
-     /* */
+  if (!h4.textContent || !destination) {
+    throw new Error("Missing destination or have a faulty h4 element")
+  }
+  destination.parentNode.insertBefore(h4, destination.nextSibling)
 }
 
-  function giveBuild(jsonObj) {
+function createYpElement(operator, cssClass) {
+  let card = document.createElement('div')
+  card.className = `yp-card b-card-light ${cssClass}`
 
-    var builders = jsonObj['rakentajat'];
-    
-    if (builders.length != 0) {
-      var hr = document.createElement('hr');
-      var br = document.createElement('br');
-  
-      var row = document.createElement('div');
-      row.className = 'row';
-      row.style.textAlign = 'center';
-  
-        for (var i = 0; i < builders.length; i++) {
-  
-          var col = document.createElement('div');
-          col.className = 'col-sm';
-          col.style.marginBottom = '20px';
-    
-          var card = document.createElement('div');
-          card.className = 'card yp-card builder';
-    
-          var cardTitle = document.createElement('h5');
-          cardTitle.className = 'card-title yp-card-title builder';
-          cardTitle.textContent = builders[i].nimi;
-    
-          var ypImg = createYpImageElement(builders[i].uuid, "builder")
-    
-          var cardBody = document.createElement('div');
-          cardBody.className = 'card-body yp-card-body builder';
-    
-          var cardText = document.createElement('p');
-          cardText.className = 'card-text';
-          cardText.textContent = builders[i].kuvaus;
-      
-          row.appendChild(col);
-          col.appendChild(card);
-          card.appendChild(cardTitle);
-          card.appendChild(ypImg);
-          card.appendChild(cardBody);
-          cardBody.appendChild(cardText);
-  
-          builder.parentNode.insertBefore(row, builder.nextSibling);
-          row.parentNode.insertBefore(hr, builder.nextSibling);
-          row.parentNode.insertBefore(br, hr.nextSibling);
-        }
-    } else {
-      var h4 = document.createElement('h4');
-      h4.innerHTML = 'Katastrofaalista! Rakentajat ovat lakossa. Jos sinulta löytyy taitoa, voisit <a id="link" href="yphaku">hakea</a>!';
-      builder.parentNode.insertBefore(h4, builder.nextSibling);
-    }
+  let cardInner = document.createElement('div')
+  cardInner.className = `yp-card-inner b-card-dark ${cssClass}`
+
+  let cardBody = document.createElement('div')
+  cardBody.className = `yp-card-body ${cssClass}`
+
+  let cardFooter = document.createElement('div')
+  cardFooter.className = `yp-card-footer ${cssClass}`
+
+  let cardHeader = document.createElement('div')
+  cardHeader.className = `yp-card-header ${cssClass}`
+
+  let cardImg = document.createElement('div')
+  cardImg.className = `yp-card-img ${cssClass}`
+
+  let cardText = document.createElement('div')
+  cardText.className = `yp-card-text ${cssClass}`
+
+  let cardRank = document.createElement('div')
+  cardRank.className = `yp-card-rank c-card-ligher ${cssClass}`
+
+  let h3 = document.createElement('h3')
+
+  let img = document.createElement('img')
+
+  let p = document.createElement('p')
+
+  let span = document.createElement('span')
+
+  h3.textContent = operator.nimi
+  p.textContent = operator.kuvaus
+  span.textContent = cssClass
+  img.src = `https://crafatar.com/renders/body/${operator.uuid}`
+
+  cardHeader.appendChild(h3)
+  cardImg.appendChild(img)
+  cardText.appendChild(p)
+  cardRank.appendChild(span)
+
+  cardBody.appendChild(cardHeader)
+  cardBody.appendChild(cardImg)
+  cardBody.appendChild(cardText)
+
+  cardFooter.appendChild(cardRank)
+
+  cardInner.appendChild(cardBody)
+  cardInner.appendChild(cardFooter)
+
+  card.appendChild(cardInner)
+
+  return card;
+}
+
+function createOperatorsDiv(operators, opType) {
+  if (operators.length === 0) {
+    createEmptyListText(opType)
+    return null;
   }
+
+  let destination = getDestination(opType);
+
+  let operatorWrapper = document.createElement('div')
+  operatorWrapper.className = 'yp-wrapper'
+
+  operators.forEach(function (operator) {
+    let opElement = createYpElement(operator, opType)
+    operatorWrapper.appendChild(opElement)
+  })
+  destination.parentNode.insertBefore(operatorWrapper, destination.nextSibling)
+}
