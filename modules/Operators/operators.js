47445aca9ccbe39b76420c1874a090bbfa76c9e6
Arttu Pennanen <arttu@pennanen.org>
2019-08-03 19:25:43 +0300

    IE korjauksia YP sivuun

--- modules/Operators/operators.js
+++ modules/Operators/operators.js
@@ -3,15 +3,19 @@ var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
-
-
+console.log("yeet")
 request.onload = function () {
   var data = request.response;
+  if (typeof data === "string") {
+    data = JSON.parse(data)
+  }
+  console.log(data)
+  console.log(typeof data)
   Object.keys(data).forEach(function (type) {
-    var list = data[type]
-    createOperatorsDiv(list, type)
-  })
-}
+    var list = data[type];
+    createOperatorsDiv(list, type);
+  });
+};
 
 var builder = document.getElementById('Rakentajat');
 var owner = document.getElementById('Omistajat');
@@ -21,127 +25,138 @@ var mod = document.getElementById('Moderaattorit');
 
 function getDestination(type) {
   var destination;
+
   switch (type) {
     case "omistajat":
-      destination = owner
+      destination = owner;
       break;
+
     case "adminit":
-      destination = admin
+      destination = admin;
       break;
+
     case "operaattorit":
-      destination = op
+      destination = op;
       break;
+
     case "moderaattorit":
-      destination = mod
+      destination = mod;
       break;
+
     case "rakentajat":
-      destination = builder
+      destination = builder;
       break;
+
     default:
-      throw new Error("Could not resolve destination element")
+      throw new Error("Could not resolve destination element");
   }
-  return destination
+
+  return destination;
 }
 
 function createEmptyListText(type, destination) {
   var h4 = document.createElement('h4');
   var destination;
+
   switch (type) {
     case "omistajat":
       h4.textContent = '#Sellout';
       break;
+
     case "adminit":
       h4.textContent = 'Ketään ei ole katsomassa Operaattoreiden perään. :(';
       break;
+
     case "operaattorit":
       h4.textContent = 'Operaattorit ovat karanneet jonnekin! Ehkä moderaattoreista voi joskus löytyä kelvollinen...';
       break;
+
     case "moderaattorit":
       h4.innerHTML = 'Oh noes! Moderators not found. Ehkä sinun kannattaisi <a id="link" href="yphaku">hakea</a>!';
       break;
+
     case "rakentajat":
       h4.innerHTML = 'Katastrofaalista! Rakentajat ovat lakossa. Jos sinulta löytyy taitoa, voisit <a id="link" href="yphaku">hakea</a>!';
       break;
+
     default:
       break;
   }
+
   if (!h4.textContent || !destination) {
-    throw new Error("Missing destination or have a faulty h4 element")
+    throw new Error("Missing destination or have a faulty h4 element");
   }
-  destination.parentNode.insertBefore(h4, destination.nextSibling)
+
+  destination.parentNode.insertBefore(h4, destination.nextSibling);
 }
 
 function createYpElement(operator, cssClass) {
-  let card = document.createElement('div')
-  card.className = `yp-card b-card-light ${cssClass}`
+  var card = document.createElement('div');
+  card.className = 'yp-card b-card-light ' + cssClass;
 
-  let cardInner = document.createElement('div')
-  cardInner.className = `yp-card-inner b-card-dark ${cssClass}`
+  var cardInner = document.createElement('div');
+  cardInner.className = 'yp-card-inner b-card-dark ' + cssClass;
 
-  let cardBody = document.createElement('div')
-  cardBody.className = `yp-card-body ${cssClass}`
+  var cardBody = document.createElement('div');
+  cardBody.className = 'yp-card-body ' + cssClass;
 
-  let cardFooter = document.createElement('div')
-  cardFooter.className = `yp-card-footer ${cssClass}`
+  var cardFooter = document.createElement('div');
+  cardFooter.className = 'yp-card-footer ' + cssClass;
 
-  let cardHeader = document.createElement('div')
-  cardHeader.className = `yp-card-header ${cssClass}`
+  var cardHeader = document.createElement('div');
+  cardHeader.className = 'yp-card-header ' + cssClass;
 
-  let cardImg = document.createElement('div')
-  cardImg.className = `yp-card-img ${cssClass}`
+  var cardImg = document.createElement('div');
+  cardImg.className = 'yp-card-img ' + cssClass;
 
-  let cardText = document.createElement('div')
-  cardText.className = `yp-card-text ${cssClass}`
+  var cardText = document.createElement('div');
+  cardText.className = 'yp-card-text ' + cssClass;
 
-  let cardRank = document.createElement('div')
-  cardRank.className = `yp-card-rank c-card-ligher ${cssClass}`
+  var cardRank = document.createElement('div');
+  cardRank.className = 'yp-card-rank c-card-ligher ' + cssClass;
 
-  let h3 = document.createElement('h3')
+  var h3 = document.createElement('h3');
+  var img = document.createElement('img');
+  var p = document.createElement('p');
+  var span = document.createElement('span');
 
-  let img = document.createElement('img')
+  h3.textContent = operator.nimi;
+  p.textContent = operator.kuvaus;
+  span.textContent = cssClass;
 
-  let p = document.createElement('p')
+  img.src = 'https://crafatar.com/renders/body/' + operator.uuid;
 
-  let span = document.createElement('span')
+  cardHeader.appendChild(h3);
+  cardImg.appendChild(img);
+  cardText.appendChild(p);
+  cardRank.appendChild(span);
 
-  h3.textContent = operator.nimi
-  p.textContent = operator.kuvaus
-  span.textContent = cssClass
-  img.src = `https://crafatar.com/renders/body/${operator.uuid}`
+  cardBody.appendChild(cardHeader);
+  cardBody.appendChild(cardImg);
+  cardBody.appendChild(cardText);
 
-  cardHeader.appendChild(h3)
-  cardImg.appendChild(img)
-  cardText.appendChild(p)
-  cardRank.appendChild(span)
+  cardFooter.appendChild(cardRank);
 
-  cardBody.appendChild(cardHeader)
-  cardBody.appendChild(cardImg)
-  cardBody.appendChild(cardText)
+  cardInner.appendChild(cardBody);
+  cardInner.appendChild(cardFooter);
 
-  cardFooter.appendChild(cardRank)
-
-  cardInner.appendChild(cardBody)
-  cardInner.appendChild(cardFooter)
-
-  card.appendChild(cardInner)
+  card.appendChild(cardInner);
 
   return card;
 }
 
 function createOperatorsDiv(operators, opType) {
   if (operators.length === 0) {
-    createEmptyListText(opType)
+    createEmptyListText(opType);
     return null;
   }
 
-  let destination = getDestination(opType);
-
-  let operatorWrapper = document.createElement('div')
-  operatorWrapper.className = 'yp-wrapper'
-
+  var destination = getDestination(opType);
+  var operatorWrapper = document.createElement('div');
+  operatorWrapper.className = 'yp-wrapper';
   operators.forEach(function (operator) {
-    let opElement = createYpElement(operator, opType)
-    operatorWrapper.appendChild(opElement)
-  })
-  destination.parentNode.insertBefore(operatorWrapper, destination.nextSibling)
+    var opElement = createYpElement(operator, opType);
+    operatorWrapper.appendChild(opElement);
+  });
+  destination.parentNode.insertBefore(operatorWrapper, destination.nextSibling);
 }
