8a9d728e5bf39efd0da52dfd31174bd25226c42c
-kakara- <kakaradarkorbit69@gmail.com>
2020-05-01 18:11:41 +0300

    Update server-status.js

--- modules/ServerStatus/server-status.js
+++ modules/ServerStatus/server-status.js
@@ -1,5 +1,5 @@
 var xmlhttp = new XMLHttpRequest();
-var url = "https://api.mcsrvstat.us/2/88.214.58.33";
+var url = "https://api.mcsrvstat.us/2/95.217.120.173";
 xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var stabox = this.responseText;
