8ca1a2946e70da6c3adc47570678cdd374fc30f4
Ixynas <kakaradarkorbit69@gmail.com>
2020-11-16 11:49:32 +0200

    v. 4.2.4
    Sisältö:
    - päivitetty muutosloki
    - päivitetty Yleistietoa -osion vanhentuneet kohdat
    - poistettu viittaukset `theme_switcher.jsx` -komponenttiin
    - lisätty Hyvä tietää -osioon emojikoodit
    
    Ulkoasu:
    - lisätty navbarin fonttiin `letter-spacing: 1px;` -tyylimääräys
    
    Tekniset muutokset:
    - `countdown.js` pitäisi nyt handlaa errorit oikein

--- modules/Countdown/countdown.js
+++ modules/Countdown/countdown.js
@@ -1,25 +1,28 @@
 // Set the date we're counting down to
 const countDownDate = new Date("Nov 6, 2020 18:00:00 GMT+02:00").getTime();
 
-let now = new Date().getTime();
-let distance = countDownDate - now;
+let atm = new Date().getTime();
+let distance = countDownDate - atm;
 
-// Update the count down every 1 second
-const x = setInterval(function() {
+// If the timespan is already passed, handle the error properly
+if (distance <= 0) {
+  try {
+    document.getElementById('mapreset').remove();
+  } catch (error) {
+    
+  }
+} else {
+  setInterval(function() {
 
-  // Time calculations for days, hours, minutes and seconds
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="mapreset"
-  document.getElementById("mapreset").innerHTML = days + " päivää " + hours + " tuntia "
-  + minutes + " minuuttia " + " ja " + seconds + " sekuntia ";
-
-}, 1000);
-
-if (distance < 0) {
-  document.getElementById("mapreset").remove();
-  clearInterval(x);
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    // Display the result in the element with id="mapreset"
+    document.getElementById("mapreset").innerHTML = days + " päivää " + hours + " tuntia "
+    + minutes + " minuuttia " + " ja " + seconds + " sekuntia ";
+  
+  }, 1000);
 }
