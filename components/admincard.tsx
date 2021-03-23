768a16dfc9e0679d6ed5ad3e2300239a52fbffcd
Arttu Pennanen <arttu@pennanen.org>
2021-03-23 03:14:07 +0200

    v. 5.0.1#2 Sisältö: - yllapito.tsx refactor - rankit.tsx refactor - style muokkailua - pohja valmis Kuolemattomien rank korteille

---
+++ components/admincard.tsx
@@ -0,0 +1,53 @@
+import style from '../styles/admincard.module.css'
+import { faArrowAltCircleRight } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const AdminStrength = ({ children }) => {
+    return (
+        <p>
+            <FontAwesomeIcon icon={faArrowAltCircleRight} className={`${style['strength-bullet']}`} />
+            {children}
+        </p>
+    )
+}
+
+const AdminCard = ({
+    rank,
+    uuid,
+    name,
+    description,
+    strengths
+}: {
+    rank: string
+    uuid: string
+    name: string
+    description: string
+    strengths: string[]
+}) => {
+    console.log(rank)
+    return (
+        <div className={`${style['card-container']} ${style[rank]}`}>
+            <div className={`${style['card']} ${style[rank]}`}>
+                <div className={`${style['background']} ${style[rank]}`}>
+                    <div className={`${style['avatar']} ${style[rank]}`}>
+                        <img src={`https://visage.surgeplay.com/full/${uuid}`} />
+                    </div>
+                </div>
+                <div className={`${style['content']} ${style[rank]}`}>
+                    <div className={`${style['type']} ${style[rank]}`}>{rank}</div>
+                    <div className={`${style['name']} ${style[rank]}`}>{name}</div>
+                    <div className={`${style['strength']} ${style[rank]}`}>
+                        {strengths.map((strength, i) => (
+                            <AdminStrength key={i}>{strength}</AdminStrength>
+                        ))}
+                    </div>
+                </div>
+                <div className={`${style['footer']}`}>
+                    <p>{description}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AdminCard
