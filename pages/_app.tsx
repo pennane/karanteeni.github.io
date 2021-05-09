c7307b5ae6234b859eb72ba91a69aff624f68740
Arttu Pennanen <arttu@pennanen.org>
2021-05-10 00:53:37 +0300

    IP to clipboard button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '../styles/global.css'
 import '../styles/summer.css'
 
+import { ToastProvider } from 'react-toast-notifications'
 import { AppProps } from 'next/app'
 
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <ToastProvider placement={'bottom-center'} portalTargetSelector={'#layout'}>
+            <Component {...pageProps} />
+        </ToastProvider>
+    )
 }
 
 export default App
