468522a000216d3ee1c02a31c61ab220c4cfbe0f
Pennane <39066923+Pennane@users.noreply.github.com>
2022-01-29 22:37:35 +0000

    Deploying to gh-pages from  @ a4ce80027e00576af19c8a2f3c40b95226e83d3a 🚀

--- _next/static/5GFqUiHGZJmGQBCvLUpGU/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fkauppa","\u002Fkauppa\u002Ftuotteet","\u002F","\u002Fjulkaisut","\u002Fkauppa\u002Ftuotteet\u002F[type]","\u002Fkauppa\u002Ftuotteet\u002F[type]\u002F[id]","\u002Fjulkaisut\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
